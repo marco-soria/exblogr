74f6e0818bac4c76891786d8a5d82446ff020db7
marco-soria <marco.soria@pucp.edu.pe>
2024-01-28 11:28:04 -0500

    blogr

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# presentationcard001
-PresentationCard Demo
+#Blogr
---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+document.getElementById("menu").addEventListener('click' ,mostrar_menu);
+
+function mostrar_menu(){
+    document.getElementById("nav").classList.toggle("ocultar-menu")
+}
